b27fdffdb88813992a1842d58ab9cb305b77dc12
Arun Sivasankaran <arunparthiban@gmail.com>
2020-12-12 20:30:42 -0800

    feat: add a test for health check

--- test/routes/time.test.ts
+++ test/routes/time.test.ts
@@ -8,7 +8,18 @@ describe('GET /time', () => {
             url: '/current-time',
         });
         expect(res.statusCode).toBe(200);
-        // todo: mock moment and test
         expect(typeof res.payload).toBe('string');
+    });
+});
+
+describe('GET /health', () => {
+    it('responds with an "all good"', async () => {
+        const server = await Server.init();
+        const res = await server.inject({
+            method: 'get',
+            url: '/health',
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.payload).toBe('all is good');
     });
 });
