3c34db05b38ae4a126cfe69501ed272e4eef3086
Arun Sivasankaran <arunparthiban@gmail.com>
2021-03-02 14:59:42 -0800

    feat: some improvements

--- src/routes/header.ts
+++ src/routes/header.ts
@@ -5,19 +5,14 @@ export const headerRoutes = {
     method: 'GET',
     path: '/headers',
     options: {
-        handler: async (_request: Hapi.Request, _h: Hapi.ResponseToolkit): Promise<any> => {
+        handler: async (_request: Hapi.Request, _h: Hapi.ResponseToolkit): Promise<Record<string, unknown>> => {
             // const timezone = _request.query.timezone as string;
             // const now = MomentTz().tz(timezone).format('HH:mm:ss');
             // return now.toString();
 
-            console.log(_request.headers);
             return _request.headers;
         },
-        description: 'returns the current time',
-        notes: [
-            'NOTE: "timezone" defaults to "America/Los_Angeles"',
-            'NOTE: The dropdown for "timezone" only list a small subset...BUT any valid timezone should work',
-        ],
+        description: 'returns the raw headers from a request',
         response: {
             schema: Joi.object(),
         },
